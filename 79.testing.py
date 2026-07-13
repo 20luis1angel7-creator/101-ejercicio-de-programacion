610a133de5f84205d6393580340b1b7d27809b6b
luis angel <20luis1angel7@gmail.com>
2026-07-13 06:55:57 -0400

    feat: 79 done

---
+++ 79.testing.py
@@ -0,0 +1,26 @@
+#  * Crea tres test sobre el reto 12: "Viernes 13".
+#  * - Puedes copiar una solución ya creada por otro usuario en
+#  *   el lenguaje que estés utilizando.
+#  * - Debes emplear un mecanismo de ejecución de test que posea
+#  *   el lenguaje de programación que hayas seleccionado.
+#  * - Los tres test deben de funcionar y comprobar
+#  *   diferentes situaciones (a tu elección).
+
+# import unittest
+
+# class TestViernes13(unittest.TestCase):
+
+#     def test_return_true(self):
+#         self.assertEqual(viernes_13(), "True")
+
+#     def test_type_return(self):
+#         self.assertIsInstance(viernes_13(), str)
+
+#     def test_value_permissibke(self):
+#         self.assertIn(viernes_13(), ["True", "False"])
+
+#     def test_is_not_false(self):
+#         self.assertNotEqual(viernes_13(), "False")
+
+# if __name__ == "__name__":
+#     unittest.main()
